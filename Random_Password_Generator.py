293a652f6374b6d125b07b042e51dfbafdc48949
TathagatChavada <chavadatathagat1234@gmail.com>
2021-07-10 21:59:42 +0530

    I have updated a file

--- Random_Password_Generator.py
+++ Random_Password_Generator.py
@@ -1,17 +1,19 @@
 import random
 import string
 
+def generate_password(x):
+    # Different characters of password
+    str1 = string.ascii_letters
+    str2 = string.digits
+    str3 = string.punctuation
+
+    str = str1 + str2 + str3
+
+    # Looping over str using random function to generate password
+    for i in range(0,l):
+        print(random.choice(str),end='')
+    print('\n')
+
 # Length of password you want to generate.
 l = int(input('Enter the length of password: '))
-
-# Different characters of password
-str1 = string.ascii_letters
-str2 = string.digits
-str3 = string.punctuation
-
-str = str1 + str2 + str3
-
-# Looping over str using random function to generate password
-for i in range(0,l):
-    print(random.choice(str),end='')
-print('\n')
+generate_password(l)
